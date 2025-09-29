f9f78dc399105bbf81315558ba45667fb22e4337
Michael <michael.elliston95@gmail.com>
2025-09-29 13:40:18 -0500

    second commit, produces unformatted output with print

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+    static Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String firstName;
+        String middleName;
+        String lastName;
+        String suffix;
+
+        System.out.println("Please enter your first name: ");
+        firstName = myScanner.nextLine();
+
+        System.out.println("Please enter the initial of your middle name, if applicable: ");
+        middleName = myScanner.nextLine();
+
+        System.out.println("Please enter your last name: ");
+        lastName = myScanner.nextLine();
+
+        System.out.println("Please enter your suffix, if applicable: ");
+        suffix = myScanner.nextLine();
+
+        System.out.printf("Hello, %s %s %s %s", firstName, middleName, lastName, suffix);
+    }
 }
